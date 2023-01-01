7e9ffec66760d2cabb00797bff416d292afc8766
Snehasrg <sghongade642@gmail.com>
2023-01-02 00:00:46 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Nonstatic_Line_Comparison;
+
+namespace NonstaticLinecomparison
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem by Non Static Method");
+         
+           // Length obj= new Length();
+            //obj.calculatelength();
+
+           // CheckEquality obj= new CheckEquality();
+            //obj.checkequallines();
+
+            LineComparison obj=new LineComparison();
+            obj.checkcomparison();
+        }
+
+    }
+}
